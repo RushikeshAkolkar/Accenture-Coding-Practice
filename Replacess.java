bb6a508b1b5301c3bdc2ddc160f60ac6c1ebc592
RushikeshAkolkar <rushikeshakolkar150@gmail.com>
2024-08-31 09:44:26 +0530

    Replacing our String With a to p , p to a File Replacess.java added

--- Replacess.java
+++ Replacess.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Replacess {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder s =new StringBuilder(sc.next());
+        String s = sc.next();
+        StringBuilder s1 =new StringBuilder();
         for(int i=0;i<s.length();i++)
         {
             if((s.charAt(i))=='a')
             {
-                s.replace("a", "p");
+                s1.append('p');
             }
             else if((s.charAt(i))=='p')
             {
-                s.replace("p", "a");
+                s1.append('a');
             }
+            else
+            {
+                s1.append(s.charAt(i));
+            }
+            
         }
-        System.out.println("After Replacing our String With a to p , p to a is         :"+s);
+        System.out.println("After Replacing our String With a to p , p to a is         :"+s1);
     }
 }
