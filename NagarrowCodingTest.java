c9643d99a95ee71402d191f7eb6f62f820d2ee4d
RushikeshAkolkar <rushikeshakolkar150@gmail.com>
2024-09-03 09:54:36 +0530

    Narrow Coding Test Ask Question reverse a String

---
+++ NagarrowCodingTest.java
@@ -0,0 +1,13 @@
+// input Goa to Delhi
+//output Delhi to Goa
+
+public class NagarrowCodingTest {
+    public static void main(String[] args) {
+        
+        int i = args.length - 1;
+        for(;i>=0;i--)
+        {
+            System.out.print(args[i]+" ");
+        }
+    }
+}
