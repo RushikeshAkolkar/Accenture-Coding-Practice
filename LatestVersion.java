93573ccf98bb63770374f7ca15db484c53830e41
RushikeshAkolkar <rushikeshakolkar150@gmail.com>
2024-09-06 11:40:31 +0530

    FInd latest version of file in given array

---
+++ LatestVersion.java
@@ -0,0 +1,39 @@
+class LatestVersion{
+    public static int latestvaersion(String[] s)
+    {
+        int version[] = new int[s.length];
+        int v = 0;
+        for(int i=0;i<s.length;i++)
+        {
+            String s1[]=s[i].split("_",s.length);
+            for(int j=1;j<s1.length;j=j+2)
+            {
+                if(s1[j-1].equals("File"))
+                {
+                    try{
+                        version[j] = Integer.valueOf(s1[j]);
+                    }
+                    catch(Exception e)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+        for(int k=1;k<version.length;k++)
+        {
+            if(version[k]>version[k-1])
+            {
+                v=version[k];
+            }
+        }
+        
+        return v;
+    }
+    public static void main(String[] args) {
+        String s[] = {"File_2","File_3","File_4","File_x"};
+        int version = latestvaersion(s);
+
+        System.out.println("The Latest Varsion is : "+version);
+    }
+}
