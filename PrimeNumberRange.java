db49d641bc90161acdead5d33cb3da0d72afa2a4
RushikeshAkolkar <rushikeshakolkar150@gmail.com>
2024-09-09 15:38:20 +0530

    Sum of prime number upto user input is in this program code  are Written

---
+++ PrimeNumberRange.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PrimeNumberRange {
+    static boolean primenumbersum(int n)
+    {
+        int sum=0;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(sum == n)
+        {
+            return true;
+        }
+        else
+        {
+        return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("how many Array Element you want in an Array");
+        int n = sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if (primenumbersum(i)) {
+                sum=sum+i;                
+            }
+        }
+        System.out.println("The Sum Of Prime Number is : "+sum);
+
+    }
+}
