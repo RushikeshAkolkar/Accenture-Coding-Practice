3f237c32e610184252c87fc6d0ccd090c5f7e596
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-09-14 14:52:11 -0700

    Amezon and Paytm Coding Round ask Question done

---
+++ FillAndWithoutFillBottol.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class FillAndWithoutFillBottol {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            int bottol=0,emptybottol=0,cnt=0;
+            System.out.println("1. Buy A Drink Bottol :");
+            System.out.println("2.Empty Drink Bottol Exchange with Filled Drink Bottols");
+            System.out.println("3.END");
+            int n=sc.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("How many bottol you want");
+                    bottol = sc.nextInt();
+                    System.out.println("You buy Bottols "+bottol);
+                    System.out.println("1. Buy A Drink Bottol :");
+                    System.out.println("2.Empty Drink Bottol Exchange with Filled Drink Bottols");
+                    System.out.println("3.END");
+                    n=sc.nextInt();
+                    break;
+                    
+                case 2:
+                    if (cnt!=0) {
+                        bottol=cnt;
+                        cnt=0;
+                        for(int i=1;i<Integer.MAX_VALUE;i++)
+                        {
+                            if (bottol>=3) {
+                                cnt++;
+                                bottol=bottol-3;                            
+                            }
+                        }
+                        
+                    }
+                    else
+                    {
+                        System.out.println("How many Empty Bottols do you have : ");
+                        emptybottol = sc.nextInt();
+                        for(int i=1;i<Integer.MAX_VALUE;i++)
+                        {
+                            if (emptybottol>=3) {
+                                cnt++;
+                                emptybottol=emptybottol-3;                            
+                            }
+                        }
+                    }
+                    System.out.println("You can Take : "+cnt);
+
+                    System.out.println("1. Buy A Drink Bottol :");
+                    System.out.println("2.Empty Drink Bottol Exchange with Filled Drink Bottols");
+                    System.out.println("3.END");
+                    n=sc.nextInt();
+                    
+                case 3:
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+}
