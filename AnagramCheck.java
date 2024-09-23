e4c0b1648e1770824193a87a826dfdfc6940d579
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-09-23 22:07:59 +0530

    Redundency from AnagramCheck.java is reduces with Arrays Collection's of java. Arrays.sort predefine method is used to sort the Array

--- AnagramCheck.java
+++ AnagramCheck.java
@@ -1,34 +1,12 @@
+import java.util.Arrays;
+
 public class AnagramCheck {
     static void checkAnagram(String s1,String s2)
     {
         char[] str1=s1.toLowerCase().toCharArray();
         char[] str2=s2.toLowerCase().toCharArray();
-        for(int i=0;i<str1.length;i++)
-        {
-            char temp;
-            for(int j=1;j<str1.length;j++)
-            {
-                if(str1[i]>str1[j])
-                {
-                    temp=str1[i];
-                    str1[i]=str1[j];
-                    str1[j]=temp;
-                }
-            }
-        }
-        for(int i=0;i<str2.length;i++)
-        {
-            char temp;
-            for(int j=1;j<str2.length;j++)
-            {
-                if(str2[i]>str2[j])
-                {
-                    temp=str2[i];
-                    str2[i]=str2[j];
-                    str2[j]=temp;
-                }
-            }
-        }
+        Arrays.sort(str2);
+        Arrays.sort(str1);
         int flag=0;
         if(str1.length==str2.length)
         {
