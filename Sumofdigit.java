84fe78990740046769caced31f29d84dfc8eab4c
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-09-24 09:24:32 +0530

    sum of two digit gievs the output as single digit

---
+++ Sumofdigit.java
@@ -0,0 +1,14 @@
+public class Sumofdigit {
+    static int Sumofdigit(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        return n%9==0?9:n%9;
+    }
+    public static void main(String[] args) {
+        int n=38;
+        System.out.println(Sumofdigit(n));
+    }
+}
