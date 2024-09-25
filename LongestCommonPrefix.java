0a6d36e9f7694e76113b1916dce34500bf3e64cc
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-09-25 11:07:21 +0530

    LongestCommonPrefix.java in this program add longest common prefix  all array element of string have

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class LongestCommonPrefix
+{
+    public static void main(String[] args) {
+        String str[]={"flower","flight","flow"};
+        System.out.println("longest Common Prefix : "+longestcommonprefix(str));
+    }
+    static String longestcommonprefix(String str[])
+    {
+        Arrays.sort(str);
+        String str1=str[0];
+        String str2=str[str.length-1];
+        int index = 0;
+        while(index<str.length)
+        {
+            if(str1.charAt(index)==str2.charAt(index))
+            {
+                index++;
+            }
+            else{
+                break;
+            }
+        }
+        return index==0?"":str1.substring(0,index);
+    }
+}
