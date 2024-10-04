c83b35354eb25c8e20465dd444b5355e545de29c
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-10-04 08:57:56 +0530

    bubblesort program is added into Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -8,15 +8,15 @@ public class Bubblesort {
     }
     static int[] bubblesort(int []nums)
     {
-        for(int i=0;i<nums.length;i++)
+        for(int i=1;i<nums.length;i++)
         {
-            for(int j=1;j<nums.length-i;j++)
+            for(int j=0;j<nums.length-i;j++)
             {
-                if(nums[j]<nums[j-1])
+                if(nums[j]>nums[j+1])
                 {
                     int temp=nums[j];
-                    nums[j]=nums[j-1];
-                    nums[j-1]=temp;
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
                 }
             }
         }
