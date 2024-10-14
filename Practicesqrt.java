8be1345821bfa66987eed935c584701e4eec8e32
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-10-14 21:59:37 +0530

    Square root of Number program is added

---
+++ Practicesqrt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practicesqrt {
+    public static void main(String[] args) {
+        System.out.println("Enter number to find sruare root : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("square root of the number is : "+Math.sqrt(n));
+    }
+}
