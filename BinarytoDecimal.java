fdf43647d89be45c1a293bcb3ffafa3857707ba0
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2024-11-14 13:59:17 +0530

    Binary to decimal and vice versa program is added

---
+++ BinarytoDecimal.java
@@ -0,0 +1,9 @@
+public class BinarytoDecimal {
+    public static void main(String[] args) {
+        String s ="1011011";int s2 = 91;
+        int i = Integer.parseInt(s,2);
+        String i2 = Integer.toString(s2,2);
+        System.out.println(i);
+        System.out.println(i2);
+    }
+}
